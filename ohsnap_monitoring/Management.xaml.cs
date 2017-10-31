39b65196bbc550a6aeaeb9dbd2dcc66a19899e0f
AlexOsiasOH <alexander.osias@onehalf.net.au>
2017-10-31 16:26:54 +0800

    management inner forms

--- ohsnap_monitoring/Management.xaml.cs
+++ ohsnap_monitoring/Management.xaml.cs
@@ -19,9 +19,24 @@ namespace ohsnap_monitoring
     /// </summary>
     public partial class Management : Window
     {
+        UserControl mgtReport = new UserControl();
+        managementForms.AssignClient assClient = new managementForms.AssignClient();
+
         public Management()
         {
             InitializeComponent();
+        }
+
+        private void mgtReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewBoxManagement.Child = null;
+            viewBoxManagement.Child = mgtReport;
+        }
+
+        private void assignClientButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewBoxManagement.Child = null;
+            //viewBoxManagement.Child = ;
         }
     }
 }
